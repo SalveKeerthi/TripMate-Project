88416cb9b21eeb0e53a45c7326705b4eeebc40b9
Salve Keerthi <salvekeerthi2005@gmail.com>
2025-07-30 23:03:31 +0530

    Create auth.js

---
+++ js/auth.js
@@ -0,0 +1,18 @@
+import { auth } from './firebase.js';
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut
+} from 'https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js';
+
+export async function registerUser(email, password) {
+  return await createUserWithEmailAndPassword(auth, email, password);
+}
+
+export async function loginUser(email, password) {
+  return await signInWithEmailAndPassword(auth, email, password);
+}
+
+export async function logoutUser() {
+  return await signOut(auth);
+}
